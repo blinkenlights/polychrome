49b6e43cb8d92081bf51e517a8379c312f4bd0b6
Luka Dornhecker <luka.dornhecker@hey.com>
2023-05-07 18:18:56 +0200

    clean up sprite tester a bit

--- octopus/lib/octopus/sprite_tester.ex
+++ octopus/lib/octopus/sprite_tester.ex
@@ -9,7 +9,7 @@ defmodule Octopus.SpriteTester do
   end
 
   def init(_) do
-    :timer.send_interval(1000, :tick)
+    :timer.send_interval(500, :tick)
 
     {:ok, sprite} =
       ExPng.Image.from_file(
@@ -25,30 +25,19 @@ defmodule Octopus.SpriteTester do
   end
 
   def handle_info(:tick, state) do
-    Broadcaster.send(%Octopus.Protobuf.Frame{
-      data: IO.iodata_to_binary(get_sprite(state.atlas, state.idx))
-    })
-
     pixels =
-      if rem(state.idx, 2) != 0 do
-        List.duplicate(0, 64)
-      else
-        get_sprite(state.atlas, trunc(state.idx / 2))
-        |> Enum.map(&:binary.bin_to_list/1)
-        |> Enum.map(&Enum.take(&1, 3))
-        |> Enum.map(&Enum.find_index(state.palette, fn value -> value == &1 end))
-      end
+      0..9
+      |> Enum.flat_map(&get_sprite(state.atlas, trunc(state.idx + &1)))
+      |> Enum.map(&Enum.take(&1, 3))
+      |> Enum.map(&Enum.find_index(state.palette, fn value -> value == &1 end))
 
     config = %Octopus.Protobuf.Config{
-      color_palette:
-        state.palette |> Enum.map(&(&1 ++ [0])) |> IO.inspect() |> IO.iodata_to_binary(),
-      easing_interval_ms: 1000,
+      color_palette: state.palette |> Enum.map(&(&1 ++ [0])) |> IO.iodata_to_binary(),
+      easing_interval_ms: 500,
       pixel_easing: :LINEAR,
       brightness_easing: :LINEAR,
       show_test_frame: false
     }
-
-    IO.inspect(pixels)
 
     frame = %Octopus.Protobuf.Frame{data: pixels |> IO.iodata_to_binary()}
 
@@ -58,13 +47,12 @@ defmodule Octopus.SpriteTester do
     {:noreply, %{state | idx: state.idx + 1}}
   end
 
-  # get the sprite at the given index
-  defp get_sprite(atlas, idx) do
-    pixels =
-      for y <- 0..7 |> Enum.reverse(), x <- 0..7 do
-        ExPng.Image.at(atlas, {x + rem(idx * 8, 128), y + trunc(idx / 16) * 8})
-      end
+  defp get_sprite(atlas, idx, flip_y \\ false) do
+    y_range = if flip_y, do: 7..0, else: 0..7
 
-    pixels
+    for y <- y_range, x <- 0..7 do
+      ExPng.Image.at(atlas, {x + rem(idx * 8, 128), y + rem(trunc(idx / 16), 16 * 8) * 8})
+    end
+    |> Enum.map(&:binary.bin_to_list/1)
   end
 end
