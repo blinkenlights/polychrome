57fa10fe9d69a028e6e6cf4f57d809162634fe28
Lukas Güldenstein <git@lukasgueldenstein.de>
2023-05-21 00:41:31 +0200

    Build golang test app and generate protobuf source with cmake

--- sonartacle/test/sonatacle-proto-sim/generate_proto.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-protoc \
-  -I=../../../protobuf \
-  --go_opt=Mschema.proto=./cmd \
-  --go_opt=Mnanopb.proto=./cmd \
-  --go_out=. \
-  ../../../protobuf/schema.proto ../../../protobuf/nanopb.proto
