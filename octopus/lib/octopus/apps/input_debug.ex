84887ff505b47c4f03718eb8b53bd41066f75fac
Dominik Wagner <wagnr@icloud.com>
2023-07-08 23:29:01 +0200

    towards showing all button states

---
+++ octopus/lib/octopus/apps/input_debug.ex
@@ -0,0 +1,263 @@
+defmodule Octopus.Apps.InputDebug do
+  use Octopus.App
+  require Logger
+
+  alias Octopus.ColorPalette
+  alias Octopus.Protobuf.{Frame, InputEvent}
+
+  @frame_rate 60
+  @frame_time_ms trunc(1000 / @frame_rate)
+
+  defmodule JoyState do
+    defstruct [:buttons]
+
+    def new() do
+      %JoyState{
+        buttons: MapSet.new()
+      }
+    end
+
+    def press(%JoyState{} = js, button) do
+      %JoyState{
+        buttons: js.buttons |> MapSet.put(button)
+      }
+    end
+
+    def release(%JoyState{} = js, button) do
+      %JoyState{
+        buttons: js.buttons |> MapSet.delete(button)
+      }
+    end
+
+    def handle_event(%JoyState{} = js, type, value) do
+      {presses, releases} =
+        cond do
+          type in [:AXIS_X_1, :AXIS_X_2] ->
+            case value do
+              1 -> {[:r], [:l]}
+              0 -> {[], [:r, :l]}
+              -1 -> {[:l], [:r]}
+            end
+
+          type in [:AXIS_Y_1, :AXIS_Y_2] ->
+            case value do
+              1 -> {[:d], [:u]}
+              0 -> {[], [:d, :u]}
+              -1 -> {[:u], [:d]}
+            end
+
+          type in [:BUTTON_1_A, :BUTTON_2_A] ->
+            case value do
+              1 -> {[:a], []}
+              _ -> {[], [:a]}
+            end
+
+          type in [:BUTTON_1_B, :BUTTON_2_B] ->
+            case value do
+              1 -> {[:b], []}
+              _ -> {[], [:b]}
+            end
+        end
+
+      new_js = Enum.reduce(releases, js, fn b, acc -> acc |> release(b) end)
+      Enum.reduce(presses, new_js, fn b, acc -> acc |> press(b) end)
+    end
+  end
+
+  defmodule ButtonState do
+    defstruct [:buttons, :joy1, :joy2]
+
+    def new() do
+      %ButtonState{
+        buttons: MapSet.new(),
+        joy1: JoyState.new(),
+        joy2: JoyState.new()
+      }
+    end
+
+    def press(%ButtonState{buttons: buttons} = bs, button) do
+      %ButtonState{bs | buttons: buttons |> MapSet.put(button)}
+    end
+
+    def release(%ButtonState{buttons: buttons} = bs, button) do
+      %ButtonState{bs | buttons: buttons |> MapSet.delete(button)}
+    end
+
+    def handle_event(%ButtonState{} = bs, type, value) do
+      case type do
+        type when type in [:AXIS_X_1, :AXIS_Y_1, :BUTTON_1_A, :BUTTON_1_B] ->
+          %ButtonState{bs | joy1: bs.joy1 |> JoyState.handle_event(type, value)}
+
+        type when type in [:AXIS_X_2, :AXIS_Y_2, :BUTTON_2_A, :BUTTON_2_B] ->
+          %ButtonState{bs | joy1: bs.joy2 |> JoyState.handle_event(type, value)}
+
+        type ->
+          case value do
+            1 -> bs |> press(type)
+            0 -> bs |> release(type)
+          end
+      end
+    end
+  end
+
+  defmodule Screen do
+    defstruct [:pixels]
+
+    def new() do
+      %Screen{
+        pixels:
+          [
+            0,
+            1,
+            2,
+            3,
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            12,
+            13,
+            14,
+            15
+          ]
+          |> Stream.cycle()
+          |> Stream.take(8 * 8)
+          |> Enum.to_list()
+      }
+    end
+
+    defp index_to_coord(i) do
+      {rem(i, 8), floor(i / 8)}
+    end
+
+    def set_pixels(%Screen{} = screen, tuples) do
+      screen_map =
+        screen.pixels
+        |> Enum.with_index()
+        |> Enum.reduce(%{}, fn {v, i}, acc ->
+          acc
+          |> Map.put(index_to_coord(i), v)
+        end)
+
+      screen_map =
+        tuples
+        |> Enum.reduce(screen_map, fn {coord, value}, acc ->
+          acc |> Map.put(coord, value)
+        end)
+
+      new_pixels =
+        0..63
+        |> Enum.reduce([], fn i, acc ->
+          [
+            screen_map
+            |> Map.get(index_to_coord(i))
+            | acc
+          ]
+        end)
+        |> Enum.reverse()
+
+      %Screen{screen | pixels: new_pixels}
+    end
+  end
+
+  defmodule State do
+    defstruct [:position, :color, :palette, :screen, :button_state]
+  end
+
+  def name(), do: "Input Debugger"
+
+  def init(_args) do
+    state = %State{
+      position: 0,
+      color: 1,
+      palette: ColorPalette.load("pico-8"),
+      screen: Screen.new(),
+      button_state: ButtonState.new()
+    }
+
+    :timer.send_interval(@frame_time_ms, :tick)
+    {:ok, state}
+  end
+
+  def handle_info(:tick, %State{} = state) do
+    render_frame(state)
+    {:noreply, state}
+  end
+
+  def handle_input(
+        %InputEvent{type: type, value: value} = event,
+        %State{button_state: bs} = state
+      ) do
+    Logger.info("Input Debug: #{inspect(event)}")
+
+    new_bs = bs |> ButtonState.handle_event(type, value) |> IO.inspect()
+
+    {:noreply, %State{state | button_state: new_bs}}
+  end
+
+  defp render_frame(%State{} = state) do
+    # collect some painting
+
+    pixel_tuples =
+      [
+        {:BUTTON_1, {0, 2}},
+        {:BUTTON_2, {1, 1}},
+        {:BUTTON_3, {2, 0}},
+        {:BUTTON_4, {2, 2}},
+        {:BUTTON_5, {3, 1}},
+        {:BUTTON_6, {4, 2}},
+        {:BUTTON_7, {5, 1}},
+        {:BUTTON_8, {6, 2}},
+        {:BUTTON_9, {6, 0}},
+        {:BUTTON_10, {7, 1}}
+      ]
+      |> Enum.reduce([], fn {b, coord}, acc ->
+        [
+          {coord,
+           if state.button_state.buttons |> MapSet.member?(b) do
+             11
+           else
+             0
+           end}
+          | acc
+        ]
+      end)
+
+    screen =
+      state.screen
+      |> Screen.set_pixels(pixel_tuples)
+
+    data =
+      screen.pixels
+      #      |> IO.inspect()
+      |> Stream.cycle()
+      |> Stream.take(8 * 8 * 10)
+      |> Enum.to_list()
+
+    %Frame{
+      data: data,
+      palette: state.palette
+    }
+    |> send_frame()
+  end
+end
--- octopus/lib/octopus/protobuf/schema.pb.ex
+++ octopus/lib/octopus/protobuf/schema.pb.ex
@@ -3,20 +3,24 @@ defmodule Octopus.Protobuf.InputType do
 
   use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :BUTTON_1, 0
-  field :BUTTON_2, 1
-  field :BUTTON_3, 2
-  field :BUTTON_4, 3
-  field :BUTTON_5, 4
-  field :BUTTON_6, 5
-  field :BUTTON_7, 6
-  field :BUTTON_8, 7
-  field :BUTTON_9, 8
-  field :BUTTON_10, 9
-  field :AXIS_X_1, 10
-  field :AXIS_Y_1, 11
-  field :AXIS_X_2, 12
-  field :AXIS_Y_2, 13
+  field(:BUTTON_1, 0)
+  field(:BUTTON_2, 1)
+  field(:BUTTON_3, 2)
+  field(:BUTTON_4, 3)
+  field(:BUTTON_5, 4)
+  field(:BUTTON_6, 5)
+  field(:BUTTON_7, 6)
+  field(:BUTTON_8, 7)
+  field(:BUTTON_9, 8)
+  field(:BUTTON_10, 9)
+  field(:AXIS_X_1, 10)
+  field(:AXIS_Y_1, 11)
+  field(:AXIS_X_2, 12)
+  field(:AXIS_Y_2, 13)
+  field(:BUTTON_1_A, 14)
+  field(:BUTTON_1_B, 15)
+  field(:BUTTON_2_A, 16)
+  field(:BUTTON_2_B, 17)
 end
 
 defmodule Octopus.Protobuf.ControlEventType do
@@ -24,10 +28,10 @@ defmodule Octopus.Protobuf.ControlEventType do
 
   use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :APP_SELECTED, 0
-  field :APP_DESELECTED, 1
-  field :APP_STARTED, 2
-  field :APP_STOPPED, 3
+  field(:APP_SELECTED, 0)
+  field(:APP_DESELECTED, 1)
+  field(:APP_STARTED, 2)
+  field(:APP_STOPPED, 3)
 end
 
 defmodule Octopus.Protobuf.EasingMode do
@@ -35,22 +39,22 @@ defmodule Octopus.Protobuf.EasingMode do
 
   use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :LINEAR, 0
-  field :EASE_IN_QUAD, 1
-  field :EASE_OUT_QUAD, 2
-  field :EASE_IN_OUT_QUAD, 3
-  field :EASE_IN_CUBIC, 4
-  field :EASE_OUT_CUBIC, 5
-  field :EASE_IN_OUT_CUBIC, 6
-  field :EASE_IN_QUART, 7
-  field :EASE_OUT_QUART, 8
-  field :EASE_IN_OUT_QUART, 9
-  field :EASE_IN_QUINT, 10
-  field :EASE_OUT_QUINT, 11
-  field :EASE_IN_OUT_QUINT, 12
-  field :EASE_IN_EXPO, 13
-  field :EASE_OUT_EXPO, 14
-  field :EASE_IN_OUT_EXPO, 15
+  field(:LINEAR, 0)
+  field(:EASE_IN_QUAD, 1)
+  field(:EASE_OUT_QUAD, 2)
+  field(:EASE_IN_OUT_QUAD, 3)
+  field(:EASE_IN_CUBIC, 4)
+  field(:EASE_OUT_CUBIC, 5)
+  field(:EASE_IN_OUT_CUBIC, 6)
+  field(:EASE_IN_QUART, 7)
+  field(:EASE_OUT_QUART, 8)
+  field(:EASE_IN_OUT_QUART, 9)
+  field(:EASE_IN_QUINT, 10)
+  field(:EASE_OUT_QUINT, 11)
+  field(:EASE_IN_OUT_QUINT, 12)
+  field(:EASE_IN_EXPO, 13)
+  field(:EASE_OUT_EXPO, 14)
+  field(:EASE_IN_OUT_EXPO, 15)
 end
 
 defmodule Octopus.Protobuf.Packet do
@@ -58,26 +62,29 @@ defmodule Octopus.Protobuf.Packet do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  oneof :content, 0
+  oneof(:content, 0)
 
-  field :frame, 2, type: Octopus.Protobuf.Frame, oneof: 0
-  field :w_frame, 3, type: Octopus.Protobuf.WFrame, json_name: "wFrame", oneof: 0
-  field :rgb_frame, 4, type: Octopus.Protobuf.RGBFrame, json_name: "rgbFrame", oneof: 0
-  field :audio_frame, 5, type: Octopus.Protobuf.AudioFrame, json_name: "audioFrame", oneof: 0
-  field :input_event, 6, type: Octopus.Protobuf.InputEvent, json_name: "inputEvent", oneof: 0
+  field(:frame, 2, type: Octopus.Protobuf.Frame, oneof: 0)
+  field(:w_frame, 3, type: Octopus.Protobuf.WFrame, json_name: "wFrame", oneof: 0)
+  field(:rgb_frame, 4, type: Octopus.Protobuf.RGBFrame, json_name: "rgbFrame", oneof: 0)
+  field(:audio_frame, 5, type: Octopus.Protobuf.AudioFrame, json_name: "audioFrame", oneof: 0)
+  field(:input_event, 6, type: Octopus.Protobuf.InputEvent, json_name: "inputEvent", oneof: 0)
 
-  field :control_event, 9,
+  field(:control_event, 9,
     type: Octopus.Protobuf.ControlEvent,
     json_name: "controlEvent",
     oneof: 0
+  )
 
-  field :firmware_config, 1,
+  field(:firmware_config, 1,
     type: Octopus.Protobuf.FirmwareConfig,
     json_name: "firmwareConfig",
     oneof: 0
+  )
 
-  field :rgb_frame_part1, 7, type: Octopus.Protobuf.RGBFrame, json_name: "rgbFramePart1", oneof: 0
-  field :rgb_frame_part2, 8, type: Octopus.Protobuf.RGBFrame, json_name: "rgbFramePart2", oneof: 0
+  field(:rgb_frame_part1, 7, type: Octopus.Protobuf.RGBFrame, json_name: "rgbFramePart1", oneof: 0)
+
+  field(:rgb_frame_part2, 8, type: Octopus.Protobuf.RGBFrame, json_name: "rgbFramePart2", oneof: 0)
 end
 
 defmodule Octopus.Protobuf.Frame do
@@ -85,9 +92,9 @@ defmodule Octopus.Protobuf.Frame do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :data, 1, type: :bytes, deprecated: false
-  field :palette, 2, type: :bytes, deprecated: false
-  field :easing_interval, 3, type: :uint32, json_name: "easingInterval"
+  field(:data, 1, type: :bytes, deprecated: false)
+  field(:palette, 2, type: :bytes, deprecated: false)
+  field(:easing_interval, 3, type: :uint32, json_name: "easingInterval")
 end
 
 defmodule Octopus.Protobuf.WFrame do
@@ -95,9 +102,9 @@ defmodule Octopus.Protobuf.WFrame do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :data, 1, type: :bytes, deprecated: false
-  field :palette, 2, type: :bytes, deprecated: false
-  field :easing_interval, 3, type: :uint32, json_name: "easingInterval"
+  field(:data, 1, type: :bytes, deprecated: false)
+  field(:palette, 2, type: :bytes, deprecated: false)
+  field(:easing_interval, 3, type: :uint32, json_name: "easingInterval")
 end
 
 defmodule Octopus.Protobuf.RGBFrame do
@@ -105,8 +112,8 @@ defmodule Octopus.Protobuf.RGBFrame do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :data, 1, type: :bytes, deprecated: false
-  field :easing_interval, 2, type: :uint32, json_name: "easingInterval"
+  field(:data, 1, type: :bytes, deprecated: false)
+  field(:easing_interval, 2, type: :uint32, json_name: "easingInterval")
 end
 
 defmodule Octopus.Protobuf.AudioFrame do
@@ -114,8 +121,8 @@ defmodule Octopus.Protobuf.AudioFrame do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :uri, 1, type: :string
-  field :channel, 2, type: :uint32
+  field(:uri, 1, type: :string)
+  field(:channel, 2, type: :uint32)
 end
 
 defmodule Octopus.Protobuf.InputEvent do
@@ -123,8 +130,8 @@ defmodule Octopus.Protobuf.InputEvent do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :type, 1, type: Octopus.Protobuf.InputType, enum: true
-  field :value, 3, type: :int32
+  field(:type, 1, type: Octopus.Protobuf.InputType, enum: true)
+  field(:value, 3, type: :int32)
 end
 
 defmodule Octopus.Protobuf.ControlEvent do
@@ -132,7 +139,7 @@ defmodule Octopus.Protobuf.ControlEvent do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :type, 1, type: Octopus.Protobuf.ControlEventType, enum: true
+  field(:type, 1, type: Octopus.Protobuf.ControlEventType, enum: true)
 end
 
 defmodule Octopus.Protobuf.FirmwareConfig do
@@ -140,11 +147,11 @@ defmodule Octopus.Protobuf.FirmwareConfig do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :luminance, 1, type: :uint32
-  field :easing_mode, 2, type: Octopus.Protobuf.EasingMode, json_name: "easingMode", enum: true
-  field :show_test_frame, 3, type: :bool, json_name: "showTestFrame"
-  field :config_phash, 4, type: :uint32, json_name: "configPhash"
-  field :enable_calibration, 5, type: :bool, json_name: "enableCalibration"
+  field(:luminance, 1, type: :uint32)
+  field(:easing_mode, 2, type: Octopus.Protobuf.EasingMode, json_name: "easingMode", enum: true)
+  field(:show_test_frame, 3, type: :bool, json_name: "showTestFrame")
+  field(:config_phash, 4, type: :uint32, json_name: "configPhash")
+  field(:enable_calibration, 5, type: :bool, json_name: "enableCalibration")
 end
 
 defmodule Octopus.Protobuf.FirmwarePacket do
@@ -152,14 +159,15 @@ defmodule Octopus.Protobuf.FirmwarePacket do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  oneof :content, 0
+  oneof(:content, 0)
 
-  field :firmware_info, 1,
+  field(:firmware_info, 1,
     type: Octopus.Protobuf.FirmwareInfo,
     json_name: "firmwareInfo",
     oneof: 0
+  )
 
-  field :remote_log, 2, type: Octopus.Protobuf.RemoteLog, json_name: "remoteLog", oneof: 0
+  field(:remote_log, 2, type: Octopus.Protobuf.RemoteLog, json_name: "remoteLog", oneof: 0)
 end
 
 defmodule Octopus.Protobuf.FirmwareInfo do
@@ -167,19 +175,19 @@ defmodule Octopus.Protobuf.FirmwareInfo do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :hostname, 1, type: :string, deprecated: false
-  field :build_time, 2, type: :string, json_name: "buildTime", deprecated: false
-  field :panel_index, 3, type: :uint32, json_name: "panelIndex"
-  field :frames_per_second, 4, type: :uint32, json_name: "framesPerSecond"
-  field :config_phash, 5, type: :uint32, json_name: "configPhash"
-  field :mac, 6, type: :string, deprecated: false
-  field :ipv4, 7, type: :string, deprecated: false
-  field :ipv6_local, 8, type: :string, json_name: "ipv6Local", deprecated: false
-  field :ipv6_global, 9, type: :string, json_name: "ipv6Global", deprecated: false
-  field :packets_per_second, 10, type: :uint32, json_name: "packetsPerSecond"
-  field :uptime, 11, type: :uint64
-  field :heap_size, 12, type: :uint32, json_name: "heapSize"
-  field :free_heap, 13, type: :uint32, json_name: "freeHeap"
+  field(:hostname, 1, type: :string, deprecated: false)
+  field(:build_time, 2, type: :string, json_name: "buildTime", deprecated: false)
+  field(:panel_index, 3, type: :uint32, json_name: "panelIndex")
+  field(:frames_per_second, 4, type: :uint32, json_name: "framesPerSecond")
+  field(:config_phash, 5, type: :uint32, json_name: "configPhash")
+  field(:mac, 6, type: :string, deprecated: false)
+  field(:ipv4, 7, type: :string, deprecated: false)
+  field(:ipv6_local, 8, type: :string, json_name: "ipv6Local", deprecated: false)
+  field(:ipv6_global, 9, type: :string, json_name: "ipv6Global", deprecated: false)
+  field(:packets_per_second, 10, type: :uint32, json_name: "packetsPerSecond")
+  field(:uptime, 11, type: :uint64)
+  field(:heap_size, 12, type: :uint32, json_name: "heapSize")
+  field(:free_heap, 13, type: :uint32, json_name: "freeHeap")
 end
 
 defmodule Octopus.Protobuf.RemoteLog do
@@ -187,5 +195,5 @@ defmodule Octopus.Protobuf.RemoteLog do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :message, 1, type: :string, deprecated: false
+  field(:message, 1, type: :string, deprecated: false)
 end
