6a61a5f5681fb3a6020d92e8d8cb2f4e36b6873a
Luka Dornhecker <luka.dornhecker@hey.com>
2023-07-13 12:14:59 +0200

    rename everything from letterbox to polychrome

--- beak/test/scripts/test-async-playback.sh
+++ beak/test/scripts/test-async-playback.sh
@@ -2,9 +2,8 @@
 
 APP=${APP:-"./build/protobuf-sim"}
 if [ -x $APP ]; then
-  $APP send playMessage -f file://Users/lukas/dev/letterbox/beak/resources/shaker.wav -c 1
-  for i in {1..10}
-  do
+  $APP send playMessage -f file://Users/lukas/dev/polychrome/beak/resources/shaker.wav -c 1
+  for i in {1..10}; do
     $APP send playMessage -f https://github.com/gueldenstone/MultiChannelSampler/raw/main/resources/arcade-notification.wav -c $(expr $i % 2 + 1)
     sleep 0.2
   done
@@ -12,4 +11,3 @@ else
   echo "${APP} is not executable"
   exit 1
 fi
-
