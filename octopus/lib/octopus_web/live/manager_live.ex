d29ec1f46b3caf07c45bfa43be3a476ac7259d4e
Luka Dornhecker <luka.dornhecker@hey.com>
2023-07-18 10:09:36 +0200

    remove unused div in manager live

--- octopus/lib/octopus_web/live/manager_live.ex
+++ octopus/lib/octopus_web/live/manager_live.ex
@@ -22,78 +22,76 @@ defmodule OctopusWeb.ManagerLive do
 
   def render(assigns) do
     ~H"""
-    <div class="w-full" phx-window-keydown="keydown-event">
-      <div class="flex w-full h-full justify-center bg-black">
-        <%= live_render(@socket, PixelsLive, id: "main") %>
+    <div class="flex w-full h-full justify-center bg-black">
+      <%= live_render(@socket, PixelsLive, id: "main") %>
+    </div>
+
+    <div class="container mx-auto">
+      <div class="border rounded m-2 p-0">
+        <div class="flex flex-row">
+          <div class="p-1 font-bold m-0 flex-grow">
+            Running:
+          </div>
+          <div>
+            <a href="/sim">
+              <button
+                class="text-slate-800 background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+              >
+                Open Sim
+              </button>
+            </a>
+          </div>
+        </div>
+        <table class="w-full text-left m-0">
+          <tbody>
+            <tr :for={
+              %{module: module, app_id: app_id, name: name, selected: selected} <- @running_apps
+            }>
+              <td class={"p-2 #{if selected, do: 'bg-slate-300 font-bold'}"}><%= name %></td>
+              <td class={"p-2 #{if selected, do: 'bg-slate-300 font-bold'}"}><%= app_id %></td>
+              <td class="flex flex-row gap-2 p-1 pl-3">
+                <button
+                  class="border py-1 px-2 rounded  bg-slate-300"
+                  phx-click="stop"
+                  phx-value-module={module}
+                  phx-value-app-id={app_id}
+                >
+                  Stop
+                </button>
+
+                <.link navigate={~p"/app/#{app_id}"} class="border py-1 px-2 rounded bg-slate-300">
+                  Configure
+                </.link>
+
+                <button
+                  :if={!selected}
+                  class="border py-1 px-2 rounded bg-slate-300"
+                  phx-click="select"
+                  phx-value-module={module}
+                  phx-value-app-id={app_id}
+                >
+                  Select
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
-      <div class="container mx-auto">
-        <div class="border rounded m-2 p-0">
-          <div class="flex flex-row">
-            <div class="p-1 font-bold m-0 flex-grow">
-              Running:
-            </div>
-            <div>
-              <a href="/sim">
-                <button
-                  class="text-slate-800 background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                  type="button"
-                >
-                  Open Sim
-                </button>
-              </a>
-            </div>
-          </div>
-          <table class="w-full text-left m-0">
-            <tbody>
-              <tr :for={
-                %{module: module, app_id: app_id, name: name, selected: selected} <- @running_apps
-              }>
-                <td class={"p-2 #{if selected, do: 'bg-slate-300 font-bold'}"}><%= name %></td>
-                <td class={"p-2 #{if selected, do: 'bg-slate-300 font-bold'}"}><%= app_id %></td>
-                <td class="flex flex-row gap-2 p-1 pl-3">
-                  <button
-                    class="border py-1 px-2 rounded  bg-slate-300"
-                    phx-click="stop"
-                    phx-value-module={module}
-                    phx-value-app-id={app_id}
-                  >
-                    Stop
-                  </button>
-
-                  <.link navigate={~p"/app/#{app_id}"} class="border py-1 px-2 rounded bg-slate-300">
-                    Configure
-                  </.link>
-
-                  <button
-                    :if={!selected}
-                    class="border py-1 px-2 rounded bg-slate-300"
-                    phx-click="select"
-                    phx-value-module={module}
-                    phx-value-app-id={app_id}
-                  >
-                    Select
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+      <div class="flex flex-col m-2">
+        <div class="p-2 font-bold">
+          Add App:
         </div>
-
-        <div class="flex flex-col m-2">
-          <div class="p-2 font-bold">
-            Add App:
-          </div>
-          <div class="border p-2 flex flex-row flex-wrap">
-            <div :for={%{module: module, name: name} <- @available_apps} class="m-0 p-1">
-              <button
-                class="border py-1 px-2 rounded bg-slate-500 text-white"
-                phx-click="start"
-                phx-value-module={module}
-              >
-                <%= name %>
-              </button>
-            </div>
+        <div class="border p-2 flex flex-row flex-wrap">
+          <div :for={%{module: module, name: name} <- @available_apps} class="m-0 p-1">
+            <button
+              class="border py-1 px-2 rounded bg-slate-500 text-white"
+              phx-click="start"
+              phx-value-module={module}
+            >
+              <%= name %>
+            </button>
           </div>
         </div>
       </div>
