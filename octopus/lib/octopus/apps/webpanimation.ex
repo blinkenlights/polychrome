cdd50dd75b5e48e375fba928299750022e13228f
Martin Wöginger <martin.woeginger@gmail.com>
2023-07-20 21:03:30 +0200

    render animation timestamps still wrong

--- octopus/lib/octopus/apps/webpanimation.ex
+++ octopus/lib/octopus/apps/webpanimation.ex
@@ -6,27 +6,51 @@ defmodule Octopus.Apps.Webpanimation do
   alias Octopus.WebP
 
   defmodule State do
-    defstruct [:animation]
+    defstruct [:frames, :width, :height]
   end
 
   def name(), do: "Webp Animation"
 
   def init(_args) do
     path = Path.join([:code.priv_dir(:octopus), "webp", "marioi-run.webp"])
-    animation = WebP.decode(path)
-    state = %State{animation: animation}
+    decoded_animation = WebP.decode(path)
+    {width, height} = decoded_animation.size
+    state = %State{frames: decoded_animation.frames, width: width, height: height}
     Process.send_after(self(), :tick, 0)
     {:ok, state}
   end
 
-  def handle_info(:tick, %State{animation: animation} = state) do
-    {pixels, timestamp} = List.first(animation.frames)
-    # canvas = Canvas.put_pixel(state.canvas, coordinates, state.color)
+  def handle_info(
+        :tick,
+        %State{frames: [animation_info | more_frames], width: width, height: height} = state
+      ) do
+    {pixels, timestamp} = animation_info
 
-    # canvas
-    # |> Canvas.to_frame()
-    # |> send_frame()
+    canvas = Canvas.new(width, height)
+    image = Enum.chunk_every(pixels, width)
 
-    {:noreply, state}
+    {canvas, _} =
+      Enum.reduce(image, {canvas, 0}, fn row, {canvas, y} ->
+        {canvas, _, y} =
+          Enum.reduce(row, {canvas, 0, y}, fn pixel, {canvas, x, y} ->
+            canvas =
+              Canvas.put_pixel(
+                canvas,
+                {x, y},
+                pixel
+              )
+
+            {canvas, x + 1, y}
+          end)
+
+        {canvas, y + 1}
+      end)
+
+    canvas
+    |> Canvas.to_frame()
+    |> send_frame()
+
+    Process.send_after(self(), :tick, timestamp)
+    {:noreply, %{state | frames: more_frames}}
   end
 end
--- octopus/lib/octopus/webp.ex
+++ octopus/lib/octopus/webp.ex
@@ -3,15 +3,7 @@ defmodule Octopus.WebP do
     otp_app: :octopus,
     crate: :octopus_webp
 
-  defstruct frames: []
+  defstruct frames: [], size: nil
 
   def decode(_path), do: :erlang.nif_error(:nif_not_loaded)
 end
-
-# type Frame = (Vec<Vec<u8>>, i32);
-
-# #[derive(Default, NifStruct)]
-# #[module = "Octopus.WebP"]
-# struct Animation {
-#     frames: Vec<Frame>,
-# }
