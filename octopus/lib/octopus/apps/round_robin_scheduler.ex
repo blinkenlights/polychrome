26edee6586f633c539743df746ca2a648ae0c308
Luka Dornhecker <luka.dornhecker@hey.com>
2023-07-23 18:34:54 +0200

    change round robin scheduler to cycle between running apps

--- octopus/lib/octopus/apps/round_robin_scheduler.ex
+++ octopus/lib/octopus/apps/round_robin_scheduler.ex
@@ -2,74 +2,99 @@ defmodule Octopus.Apps.RoundRobinScheduler do
   use Octopus.App
   require Logger
 
+  alias Octopus.Mixer
   alias Octopus.AppSupervisor
 
   defmodule State do
-    defstruct [:apps, :index, :running_since]
+    defstruct [:selected_app]
   end
 
-  @max_running_time 600_000
+  @max_running_time 300_000
 
   def name(), do: "RoundRobinScheduler"
 
   def init(_args) do
-    state = %State{
-      apps: [
-        Octopus.Apps.PixelFun,
-        Octopus.Apps.MarioRun,
-        Octopus.Apps.Sprites,
-        Octopus.Apps.Webpanimation
-      ],
-      index: -1,
-      running_since: nil
-    }
+    AppSupervisor.subscribe()
+    Mixer.subscribe()
 
-    # subscribe()
-    Process.send_after(self(), :tick, 0)
-    {:ok, state}
+    Logger.metadata(scheduler: "RoundRobinScheduler")
+
+    selected_app = Mixer.get_selected_app()
+
+    Process.send_after(self(), :next_app, @max_running_time)
+
+    {:ok, %State{selected_app: selected_app}}
   end
 
-  def handle_info(:tick, %State{} = state) do
-    if state.index >= 0 do
-      state.apps
-      |> Enum.at(state.index)
-      |> app_id_of_running_app()
-      |> AppSupervisor.stop_app()
+  def handle_info(:next_app, %State{} = state) do
+    Logger.debug("next_app")
+    state = select_next_app(state)
+    Process.send_after(self(), :next_app, @max_running_time)
+    {:noreply, state}
+  end
+
+  def handle_info({:apps, {:stopped, app_id}}, %State{selected_app: app_id} = state) do
+    Logger.info("meh")
+    state = %State{state | selected_app: nil} |> select_next_app() |> IO.inspect()
+    {:noreply, state}
+  end
+
+  def handle_info({:apps, msg}, %State{} = state) do
+    Logger.info({msg, state} |> inspect())
+    {:noreply, state}
+  end
+
+  def handle_info({:mixer, {:selected_app, app_id}}, %State{} = state) do
+    {:noreply, %State{state | selected_app: app_id}}
+  end
+
+  def handle_info({:mixer, _}, %State{} = state) do
+    {:noreply, state}
+  end
+
+  defp select_next_app(%State{selected_app: nil} = state) do
+    case get_running_apps() do
+      [] ->
+        Logger.info("No apps to select from")
+        state
+
+      [head | _tail] ->
+        Mixer.select_app(head)
+        %State{state | selected_app: head}
     end
-
-    Process.send_after(self(), :tick, @max_running_time)
-    state = start_next_app(state)
-    {:noreply, state}
   end
 
-  def handle_info({:apps, {:stopped, _app_id, _module}}, state) do
-    state = start_next_app(state)
-    {:noreply, state}
+  defp select_next_app(%State{} = state) do
+    Logger.info("Selecting next app")
+
+    case get_running_apps() do
+      [] ->
+        Logger.info("No apps to select from")
+        state
+
+      [app_id] ->
+        Mixer.select_app(app_id)
+        %State{state | selected_app: app_id}
+        state
+
+      apps ->
+        next_app_id =
+          (apps ++ apps)
+          |> Enum.drop_while(fn app_id -> app_id != state.selected_app end)
+          |> Enum.at(1)
+
+        Logger.debug("Selecting #{next_app_id}")
+
+        Mixer.select_app(next_app_id)
+
+        %State{state | selected_app: next_app_id}
+    end
   end
 
-  defp start_next_app(state) do
-    new_index = rem(state.index + 1, Enum.count(state.apps))
-
-    new_app = Enum.at(state.apps, new_index)
-    AppSupervisor.start_app(new_app)
-
-    new_app
-    |> app_id_of_running_app()
-    |> Octopus.Mixer.select_app()
-
-    %State{state | index: new_index, running_since: Time.utc_now()}
-  end
-
-  defp subscribe() do
-    Phoenix.PubSub.subscribe(Octopus.PubSub, "apps")
-  end
-
-  defp app_id_of_running_app(app_module_to_look_for) do
-    running_apps = AppSupervisor.running_apps()
-
-    {_, id} =
-      Enum.find(running_apps, fn {app_module, _app_id} -> app_module == app_module_to_look_for end)
-
-    id
+  defp get_running_apps do
+    AppSupervisor.running_apps()
+    |> Enum.map(fn {_module, app_id} -> app_id end)
+    |> Enum.reject(fn app_id -> app_id == AppSupervisor.lookup_app_id(self()) end)
+    |> Enum.sort_by(fn app_id -> app_id end)
   end
 end
