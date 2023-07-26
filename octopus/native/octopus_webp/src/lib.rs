9903d40d67ce9bef8cf59392cac8f34b9a1440ec
Luka Dornhecker <luka.dornhecker@hey.com>
2023-07-26 12:14:50 +0200

    remove example function from webp code

--- octopus/native/octopus_webp/src/lib.rs
+++ octopus/native/octopus_webp/src/lib.rs
@@ -1,11 +1,6 @@
 use rustler::NifStruct;
 use webp_animation::{ColorMode, Decoder};
 
-#[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
-}
-
 type Frame = (Vec<Vec<u8>>, i32);
 
 #[derive(Default, NifStruct)]
