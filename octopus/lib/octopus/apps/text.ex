80d003954164c9074dc4585a3e54300dbd26d886
Luka Dornhecker <luka.dornhecker@hey.com>
2023-08-14 20:02:35 +0200

    add click sound to text animation

--- octopus/lib/octopus/apps/text.ex
+++ octopus/lib/octopus/apps/text.ex
@@ -2,15 +2,15 @@ defmodule Octopus.Apps.Text do
   use Octopus.App, category: :animation
   require Logger
 
+  alias Octopus.Protobuf.AudioFrame
   alias Octopus.{Canvas, Font, Transitions}
 
   @animation_steps 50
   @animation_interval 15
-  @letter_delay 5
   @easing_interval 150
 
   defmodule State do
-    defstruct [:text, :variant, :font, :animation, :easing_interval]
+    defstruct [:text, :variant, :font, :animation, :easing_interval, :letter_delay, :click]
   end
 
   def name(), do: "Text"
@@ -26,13 +26,15 @@ defmodule Octopus.Apps.Text do
     %{
       text: {"Text", :string, %{default: "POLYCHROME"}},
       font: {"Font", :string, %{default: "ddp-DoDonPachi (Cave)"}},
+      letter_delay: {"Letter Delay", :int, %{default: 5, min: 1, max: 100}},
+      click: {"Click", :boolean, %{default: false}},
       variant: {"Variant", :int, %{default: 0}},
       easing_interval: {"Easing Interval", :int, %{default: 500, min: 0, max: 3000}}
     }
   end
 
   def get_config(%State{} = state) do
-    Map.take(state, [:text, :font, :variant, :easing_interval])
+    Map.take(state, [:text, :font, :variant, :easing_interval, :letter_delay, :click])
   end
 
   def handle_config(config, %State{} = state) do
@@ -49,17 +51,35 @@ defmodule Octopus.Apps.Text do
     |> Enum.with_index()
     |> Enum.map(fn {char, index} ->
       final = Font.draw_char(font, char, state.variant, empty_window)
-      padding_start = List.duplicate(empty_window, index * @letter_delay)
-      padding_end = List.duplicate(final, (9 - index) * @letter_delay)
+      padding_start = List.duplicate(empty_window, index * state.letter_delay)
+      padding_end = List.duplicate(final, (9 - index) * state.letter_delay)
+
+      padding_end =
+        if state.click && length(padding_end) > 0 do
+          [padding_end_head | padding_end_tail] = padding_end
+          [{padding_end_head, :click, index + 1} | padding_end_tail]
+        else
+          padding_end
+        end
+
       transition = Transitions.push(empty_window, final, direction: :top, steps: @animation_steps)
       Stream.concat([padding_start, transition, padding_end])
     end)
     |> Stream.zip()
-    |> Stream.map(fn tuple ->
-      Tuple.to_list(tuple)
-      |> Enum.reverse()
-      |> Enum.reduce(&Canvas.join/2)
-      |> Canvas.to_frame(easing_interval: @easing_interval)
+    |> Stream.map(fn
+      tuple ->
+        Tuple.to_list(tuple)
+        |> Enum.reverse()
+        |> Enum.map(fn
+          {canvas, :click, channel} ->
+            send_frame(%AudioFrame{uri: "file://ui/switch3.wav", channel: channel})
+            canvas
+
+          canvas ->
+            canvas
+        end)
+        |> Enum.reduce(&Canvas.join/2)
+        |> Canvas.to_frame(easing_interval: @easing_interval)
     end)
     |> Stream.map(fn frame ->
       :timer.sleep(@animation_interval)
