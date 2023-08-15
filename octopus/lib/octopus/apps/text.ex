8939661ea39e46bdef7f464d725e85f8a986380f
reimerei <git@bjrm.de>
2023-08-15 18:50:54 +0200

    add option to configure animation for text app

--- octopus/lib/octopus/apps/text.ex
+++ octopus/lib/octopus/apps/text.ex
@@ -2,24 +2,37 @@ defmodule Octopus.Apps.Text do
   use Octopus.App, category: :animation
   require Logger
 
-  alias Octopus.Protobuf.AudioFrame
+  alias Octopus.Protobuf.{AudioFrame, ControlEvent}
   alias Octopus.{Canvas, Font, Transitions}
 
   @animation_steps 50
-  @animation_interval 15
+  @animation_interval 5
   @easing_interval 150
 
   defmodule State do
-    defstruct [:text, :variant, :font, :animation, :easing_interval, :letter_delay, :click]
+    defstruct [
+      :text,
+      :variant,
+      :font,
+      :animation,
+      :easing_interval,
+      :letter_delay,
+      :click,
+      :transition
+    ]
   end
 
   def name(), do: "Text"
 
   def init(config) do
     state = struct(State, config)
-    send(self(), :tick)
 
     {:ok, state}
+  end
+
+  def handle_control_event(%ControlEvent{type: :APP_SELECTED}, state) do
+    send(self(), :tick)
+    {:noreply, state}
   end
 
   def config_schema() do
@@ -29,12 +42,13 @@ defmodule Octopus.Apps.Text do
       letter_delay: {"Letter Delay", :int, %{default: 5, min: 1, max: 100}},
       click: {"Click", :boolean, %{default: false}},
       variant: {"Variant", :int, %{default: 0}},
+      transition: {"Transition", :string, %{default: "push"}},
       easing_interval: {"Easing Interval", :int, %{default: 500, min: 0, max: 3000}}
     }
   end
 
   def get_config(%State{} = state) do
-    Map.take(state, [:text, :font, :variant, :easing_interval, :letter_delay, :click])
+    Map.take(state, [:text, :font, :variant, :easing_interval, :letter_delay, :click, :transition])
   end
 
   def handle_config(config, %State{} = state) do
@@ -51,19 +65,31 @@ defmodule Octopus.Apps.Text do
     |> Enum.with_index()
     |> Enum.map(fn {char, index} ->
       final = Font.draw_char(font, char, state.variant, empty_window)
-      padding_start = List.duplicate(empty_window, index * state.letter_delay)
-      padding_end = List.duplicate(final, (9 - index) * state.letter_delay + 1)
 
-      padding_end =
-        if state.click do
-          [padding_end_head | padding_end_tail] = padding_end
-          [{padding_end_head, :click, index + 1} | padding_end_tail]
-        else
-          padding_end
-        end
+      case state.transition do
+        "none" ->
+          [final]
 
-      transition = Transitions.push(empty_window, final, direction: :top, steps: @animation_steps)
-      Stream.concat([padding_start, transition, padding_end])
+        "flipdot" ->
+          Transitions.flipdot(empty_window, final)
+
+        _ ->
+          padding_start = List.duplicate(empty_window, index * state.letter_delay)
+          padding_end = List.duplicate(final, (9 - index) * state.letter_delay + 1)
+
+          padding_end =
+            if state.click do
+              [padding_end_head | padding_end_tail] = padding_end
+              [{padding_end_head, :click, index + 1} | padding_end_tail]
+            else
+              padding_end
+            end
+
+          transition =
+            Transitions.push(empty_window, final, direction: :top, steps: @animation_steps)
+
+          Stream.concat([padding_start, transition, padding_end])
+      end
     end)
     |> Stream.zip()
     |> Stream.map(fn
