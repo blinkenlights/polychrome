ae2c7c9acd3150d69ca8753554d27a4c8cb29942
reimerei <git@bjrm.de>
2023-08-15 18:50:24 +0200

    Find fonts based on the name prefix

--- octopus/lib/octopus/font.ex
+++ octopus/lib/octopus/font.ex
@@ -24,29 +24,23 @@ defmodule Octopus.Font do
   """
   def load(name) do
     Cachex.fetch!(__MODULE__, name, fn _ ->
-      path = Path.join([:code.priv_dir(:octopus), "fonts", "#{name}.png"])
+      path = find_font_path(name)
+      {:ok, img} = ExPng.Image.from_file(path)
 
-      if File.exists?(path) do
-        {:ok, img} = ExPng.Image.from_file(path)
-
-        variants =
-          img.pixels
-          |> to_rgb()
+      variants =
+        img.pixels
+        |> to_rgb()
+        |> Enum.chunk_every(8)
+        |> Enum.map(&List.flatten/1)
+        |> Enum.map(fn variant ->
+          variant
           |> Enum.chunk_every(8)
+          |> Enum.chunk_every(div(img.width, 8))
+          |> Enum.zip_with(&Function.identity/1)
           |> Enum.map(&List.flatten/1)
-          |> Enum.map(fn variant ->
-            variant
-            |> Enum.chunk_every(8)
-            |> Enum.chunk_every(div(img.width, 8))
-            |> Enum.zip_with(&Function.identity/1)
-            |> Enum.map(&List.flatten/1)
-          end)
+        end)
 
-        {:commit, %__MODULE__{name: name, variants: variants}}
-        # %__MODULE__{name: name, variants: variants}
-      else
-        raise "Font #{path} not found"
-      end
+      {:commit, %__MODULE__{name: name, variants: variants}}
     end)
   end
 
@@ -96,5 +90,16 @@ defmodule Octopus.Font do
 
   defp to_rgb([<<r, g, b, 255>> | tl]) do
     [{r, g, b} | to_rgb(tl)]
+  end
+
+  defp find_font_path(name) do
+    font_dir = :code.priv_dir(:octopus) |> Path.join("fonts")
+
+    File.ls!(font_dir)
+    |> Enum.find(&String.starts_with?(&1, name))
+    |> case do
+      nil -> raise "Font #{name} not found"
+      filename -> Path.join(font_dir, filename)
+    end
   end
 end
