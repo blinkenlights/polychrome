2bcf45e5c7f561a364bdc6abffe08bc8140b7388
Dominik Wagner <wagnr@icloud.com>
2023-08-18 23:08:28 +0200

    intermediate blocks step

--- octopus/lib/octopus/apps/blocks/game.ex
+++ octopus/lib/octopus/apps/blocks/game.ex
@@ -1,16 +1,93 @@
 defmodule Octopus.Apps.Blocks.Game do
-  defstruct score: 0, layout: %{}, moved: false, t: 0
+  alias Octopus.Canvas
+  defstruct tile: nil, board: Canvas.new(8, 16), score: 0, layout: %{}, moved: false, t: 0
+  alias Phoenix.LiveDashboard.TitleBarComponent
   alias Octopus.Font
   alias Octopus.Sprite
-  alias Octopus.Canvas
   alias Octopus.JoyState
   alias Octopus.Apps.Blocks
   alias Blocks.Game
+
+  defmodule Tile do
+    defstruct pos: {3, -2}, index: 0, rotations: [], col: nil, t_rem: 20
+
+    def new(rotations, col) do
+      %Tile{
+        rotations: rotations,
+        col: col
+      }
+    end
+
+    def tile_canvas(%Tile{index: index, rotations: rotations, col: col}) do
+      rotations
+      |> Enum.at(index)
+      |> Enum.with_index()
+      |> Enum.reduce(Canvas.new(4, 4), fn {row, y}, acc ->
+        row
+        |> Enum.with_index()
+        |> Enum.reduce(acc, fn
+          {0, _}, acc -> acc
+          {1, x}, acc -> acc |> Canvas.put_pixel({x, y}, col)
+        end)
+      end)
+    end
+
+    def move(%Tile{pos: {x, y}} = tile) do
+      %Tile{
+        tile
+        | pos: {x, y + 1}
+      }
+    end
+  end
+
+  @tiles Code.eval_string("""
+         [
+         [
+         [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 1, 1],
+         [0, 1, 1, 0]],
+         [[0, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 1, 0],
+         [0, 0, 1, 0]]
+         ],
+         [
+           [[0, 1, 0, 0],
+           [0, 1, 0, 0],
+           [0, 1, 0, 0],
+           [0, 1, 0, 0]],
+           [[0, 0, 0, 0],
+           [0, 0, 0, 0],
+           [1, 1, 1, 1],
+           [0, 0, 0, 0]]
+           ],
+         ]
+         """)
+         |> elem(0)
+
+  @colors [
+    {219, 59, 57},
+    {235, 155, 62},
+    {248, 226, 76},
+    {106, 181, 85},
+    {84, 170, 221},
+    {36, 87, 153},
+    {135, 47, 136}
+  ]
+
+  def new_tile() do
+    Tile.new(
+      @tiles |> Enum.random(),
+      @colors |> Enum.random()
+    )
+  end
 
   def new(args) do
     title = Canvas.from_string("T", Font.load("robot"))
 
     %Game{
+      tile: new_tile(),
       layout:
         case args[:layout] do
           nil ->
@@ -18,8 +95,9 @@ defmodule Octopus.Apps.Blocks.Game do
               %{
                 base_canvas: Canvas.new(40, 8) |> Canvas.overlay(title),
                 score_base: 16,
-                playfield_base: 8 * 4,
-                playfield_channel: 5
+                playfield_base: 8 * 3,
+                playfield_channel: 5,
+                playfield_rotation: :ccw
               }
             else
               %{
@@ -28,7 +106,8 @@ defmodule Octopus.Apps.Blocks.Game do
                   |> Canvas.overlay(title, offset: {4 * 8, 0}),
                 score_base: 24,
                 playfield_base: 0,
-                playfield_channel: 6
+                playfield_channel: 6,
+                playfield_rotation: :cw
               }
             end
 
@@ -41,7 +120,7 @@ defmodule Octopus.Apps.Blocks.Game do
   def tick(%Game{} = game, _joy) do
     %Game{
       case rem(game.t, 60) do
-        0 -> %Game{game | score: game.score + 1}
+        0 -> %Game{game | score: game.score + 1, tile: game.tile |> Tile.move()}
         _ -> game
       end
       | t: game.t + 1
@@ -55,9 +134,14 @@ defmodule Octopus.Apps.Blocks.Game do
     font = Font.load("robot")
     font_variant = 8
 
+    gamecanvas =
+      game.board
+      |> Canvas.overlay(Tile.tile_canvas(game.tile))
+      |> Canvas.rotate(layout.playfield_rotation)
+
     canvas =
       layout.base_canvas
-      #      |> Canvas.overlay(gamecanvas, offset: {layout.playfield_base, 0})
+      |> Canvas.overlay(gamecanvas, offset: {layout.playfield_base, 0})
       |> Font.pipe_draw_char(font, second, font_variant, {layout.score_base, 0})
       |> (fn c ->
             unless first == ?0 do
