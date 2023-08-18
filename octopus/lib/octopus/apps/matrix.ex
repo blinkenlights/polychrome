8e39c518719600f6e3230f919aed9c67d8e32ca2
Luka Dornhecker <luka.dornhecker@hey.com>
2023-08-18 21:17:42 +0200

    wip

--- octopus/lib/octopus/apps/matrix.ex
+++ octopus/lib/octopus/apps/matrix.ex
@@ -2,13 +2,14 @@ defmodule Octopus.Apps.Matrix do
   use Octopus.App, category: :animation
 
   defmodule Particle do
-    defstruct [:x, :y, :speed, :color, :age, :max_age]
+    defstruct [:x, :y, :z, :speed, :color, :age, :max_age, :tail]
 
     # Returns an rgb tuple for the particle dependent on its age and max_age
     # The color is a shade of green
-    def color(%Particle{age: age, max_age: max_age, x: x, y: y, color: {r, g, b}}, factors) do
-      factor = Map.get(factors, {trunc(x), trunc(y)}, 1 - age / max_age)
-      {trunc(r * factor) |> max(0), trunc(g * factor) |> max(0), trunc(b * factor) |> max(0)}
+    def color(%Particle{age: age, max_age: max_age, x: x, y: y, z: z, color: {r, g, b}}, factors) do
+      # factor = Map.get(factors, {trunc(x), trunc(y)}, 1 - age / max_age)
+      # {trunc(r * factor) |> max(0), trunc(g * factor) |> max(0), trunc(b * factor) |> max(0)}
+      {trunc(r * z) |> max(0), trunc(g * z) |> max(0), trunc(b * z) |> max(0)}
     end
   end
 
@@ -23,10 +24,15 @@ defmodule Octopus.Apps.Matrix do
           %Particle{
             x: :rand.uniform(80),
             y: :rand.uniform(8) - 12,
+            z: :rand.uniform() * 0.5 + 0.5,
             speed: 6.0 + :rand.uniform() * 10.0,
             age: 0.0,
             max_age: 3 + :rand.uniform() * 3,
-            color: {:rand.uniform(30), 220 + :rand.uniform(30), :rand.uniform(30)}
+            tail:
+              Enum.map(1..(4 + :rand.uniform(3)), fn _ ->
+                Enum.random([{:rand.uniform(20), 160 + :rand.uniform(55), :rand.uniform(20)}])
+              end),
+            color: {:rand.uniform(40), 200 + :rand.uniform(55), :rand.uniform(40)}
           }
         end)
 
@@ -57,17 +63,13 @@ defmodule Octopus.Apps.Matrix do
 
       canvas =
         particles
-        |> Enum.sort_by(fn %Particle{y: y} -> y end)
+        |> Enum.sort_by(fn %Particle{z: z} -> z end)
         |> Enum.reduce(canvas, fn %Particle{x: x, y: y, age: age} = particle, canvas ->
-          trail_length = 8
-
           canvas =
-            Enum.reduce(1..trail_length, canvas, fn i, canvas ->
-              Canvas.put_pixel(
-                canvas,
-                {trunc(x), trunc(y - i)},
-                Particle.color(%Particle{particle | age: age + i * 0.2}, factors)
-              )
+            particle.tail
+            |> Enum.with_index()
+            |> Enum.reduce(canvas, fn {color, i}, canvas ->
+              Canvas.put_pixel(canvas, {x, y - (i + 1)}, color)
             end)
 
           Canvas.put_pixel(canvas, {trunc(x), trunc(y)}, {150, 255, 150})
