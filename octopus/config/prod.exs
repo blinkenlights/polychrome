8a6113248f00ecfcffed867dc00d79125f21493a
reimerei <gi@bjrm.de>
2024-07-18 22:36:57 +0200

    set log level to debug

--- octopus/config/prod.exs
+++ octopus/config/prod.exs
@@ -24,7 +24,7 @@ config :octopus,
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Octopus.Finch
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
