4634ca07c685a839297079f9eb6691a722a3861c
Michaela <michaela.lehr@geildanke.com>
2025-07-12 15:55:09 +0200

    feat: button press loads random image

--- octopus/lib/octopus/apps/shape_match.ex
+++ octopus/lib/octopus/apps/shape_match.ex
@@ -20,7 +20,6 @@ defmodule Octopus.Apps.ShapeMatch do
     Octopus.App.configure_display(layout: :adjacent_panels)
 
     animation_files = ["balls1", "balls2", "balls3"]
-
     # Load all animations
     loaded_animations =
       animation_files
@@ -78,6 +77,39 @@ defmodule Octopus.Apps.ShapeMatch do
     {:noreply, %{state | frame_index: frame_index + 1}}
   end
 
+  def handle_event(
+    %Octopus.Events.Event.Input{type: :button, action: :press, button: button},
+    %{chosen_per_panel: chosen_per_panel, installation_info: installation_info} = state
+  ) do
+    panel_count = installation_info.panel_count
+    Logger.info("Button pressed")
+    if button >= 0 and button <= panel_count do
+      button_index = button - 1
+      animation_files = ["balls1", "balls2", "balls3"]
+      # Load all animations
+      loaded_animations =
+        animation_files
+        |> Enum.map(fn name ->
+          {name, Octopus.WebP.load_animation(name)}
+        end)
+      # Randomly pick one for this panel
+      new_animation = Enum.random(loaded_animations)
+      # Replace the animation at button_index
+      updated_chosen_per_panel =
+        List.replace_at(chosen_per_panel, button_index, new_animation)
+      Logger.info("Button #{button} pressed → new animation for panel #{button_index}")
+      {:noreply, %{state | chosen_per_panel: updated_chosen_per_panel}}
+    else
+      # Button outside valid range
+      {:noreply, state}
+    end
+  end
+
+  def handle_event(event, state) do
+    Logger.info("Received event: #{inspect(event)}")
+    Logger.info("Event module: #{inspect(event.__struct__)}")
+    {:noreply, state}
+  end
 
   def handle_event(_, state) do
     {:noreply, state}
