95a87aa6796106c7df9e205caff19e72f727bafb
Martin Wöginger <martin.woeginger@gmail.com>
2025-07-13 18:34:04 +0200

    demo app with proximity events
    * use events instead of directly reading the data
    * lower the spike to avoid getting negative colour values at the end 
    because the distance gets larger than the expexted max value

--- octopus/lib/octopus/apps/proximity_demo.ex
+++ octopus/lib/octopus/apps/proximity_demo.ex
@@ -3,6 +3,7 @@ defmodule Octopus.Apps.ProximityDemo do
   require Logger
 
   alias Octopus.Canvas
+  alias Octopus.Events.Event.Proximity, as: ProximityEvent
 
   defmodule State do
     defstruct [:min_distance, :max_distance]
@@ -10,10 +11,6 @@ defmodule Octopus.Apps.ProximityDemo do
 
   @fps 30
   @frame_time_ms trunc(1000 / @fps)
-
-  # Smoothing factor for exponential moving average (0.0 to 1.0)
-  # Lower values = more smoothing, higher values = more responsive
-  # @smoothing_factor 0.1
 
   def name(), do: "Proximity Demo"
 
@@ -39,9 +36,6 @@ defmodule Octopus.Apps.ProximityDemo do
     # Configure display using new unified API - adjacent layout for proximity sensors
     Octopus.App.configure_display(layout: :adjacent_panels)
 
-    # Start the timer for rendering
-    :timer.send_interval(@frame_time_ms, :tick)
-
     {:ok,
      %State{
        min_distance: config.min_distance,
@@ -49,46 +43,46 @@ defmodule Octopus.Apps.ProximityDemo do
      }}
   end
 
-  def handle_info(:tick, %State{} = state) do
+  def handle_event(%ProximityEvent{} = event, state) do
     state
-    |> render_proximity_data()
+    |> render_proximity_data(event)
     |> Octopus.App.update_display()
 
     {:noreply, state}
   end
 
-  defp render_proximity_data(%State{min_distance: min, max_distance: max}) do
+  def handle_event(_any_event, state) do
+    {:noreply, state}
+  end
+
+  defp render_proximity_data(
+         %State{min_distance: min, max_distance: max},
+         %ProximityEvent{distance: distance, panel: panel_index, sensor: sensor_index} = event
+       ) do
     # Get display info to use correct dimensions
     display_info = Octopus.App.get_display_info()
     canvas = Canvas.new(display_info.width, display_info.height)
+    brightness_ratio = 1.0 - Enum.min([0.99, (distance - min) / (max - min)])
+    brightness_value = trunc(brightness_ratio * 100)
 
-    # Get all smoothed readings from the ProximitySensor
-    # smoothed_measurements = Octopus.ProximitySensor.get_smoothed_values()
-    smoothed_measurements = []
+    %Chameleon.RGB{r: r, g: g, b: b} =
+      Chameleon.HSV.new(280, 100, brightness_value)
+      |> Chameleon.convert(Chameleon.RGB)
 
-    Enum.reduce(smoothed_measurements, canvas, fn {{panel_index, sensor_index}, distance},
-                                                  acc_canvas ->
-      brightness_ratio = 1.0 - (distance - min) / (max - min)
-      brightness_value = trunc(brightness_ratio * 100)
+    color = {r, g, b}
 
-      %Chameleon.RGB{r: r, g: g, b: b} =
-        Chameleon.HSV.new(280, 100, brightness_value)
-        |> Chameleon.convert(Chameleon.RGB)
+    panel_start_x = panel_index * display_info.panel_width
+    side_width = div(display_info.panel_width, 2)
 
-      color = {r, g, b}
+    # Sensor 0 = left side, Sensor 1 = right side
+    x_start = panel_start_x + if sensor_index == 0, do: 0, else: side_width
+    x_end = x_start + side_width - 1
 
-      panel_start_x = panel_index * display_info.panel_width
-      side_width = div(display_info.panel_width, 2)
-
-      # Sensor 0 = left side, Sensor 1 = right side
-      x_start = panel_start_x + if sensor_index == 0, do: 0, else: side_width
-      x_end = x_start + side_width - 1
-
-      for x <- x_start..x_end,
-          y <- 0..(display_info.panel_height - 1),
-          reduce: acc_canvas do
-        canvas -> Canvas.put_pixel(canvas, {x, y}, color)
-      end
-    end)
+    for x <- x_start..x_end,
+        y <- 0..(display_info.panel_height - 1),
+        reduce: canvas do
+      canvas ->
+        Canvas.put_pixel(canvas, {x, y}, color)
+    end
   end
 end
