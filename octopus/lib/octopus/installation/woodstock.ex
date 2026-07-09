f5eb5cf30a483556e4ba50d97325420bb1ac6775
Tim Pritlove <tim@pritlove.org>
2026-07-09 16:42:58 +0200

    new installation woodstock

---
+++ octopus/lib/octopus/installation/woodstock.ex
@@ -0,0 +1,27 @@
+defmodule Octopus.Installation.Woodstock do
+  use Octopus.Installation,
+    arrangement: :linear,
+    panels: [
+      [controller: :polychrome_panel_9, wiring: :serpentine_vertical_bottom_left],
+      [controller: :polychrome_panel_10, wiring: :serpentine_vertical_bottom_left]
+    ],
+    panel_layout: {2, 32},
+    num_buttons: 0,
+    num_joysticks: 0,
+    panel_gap: 0,
+    global_speed: 1.0,
+    radar_enabled: false,
+    location: :auto,
+    auto_brightness: false,
+    network_config: [
+      mode: :individual,
+      send_in_dev: true
+    ],
+    simulator_layouts: [
+      [
+        name: "Woodstock",
+        mode: "generic",
+        pixel_size: {16, 16}
+      ]
+    ]
+end
